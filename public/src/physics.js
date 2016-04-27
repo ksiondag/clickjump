ad92b4e12473b1146b3a2a09bdd5bc1547a07a51
Gregory Ksionda <ksiondag846@gmail.com>
2016-04-27 15:25:35 -0700

    Writing some post-mortem-esque alternative ideas

--- public/src/physics.js
+++ public/src/physics.js
@@ -4,6 +4,18 @@ var calculateVelocity = function (dx, dy) {
     var dt, vy, vx;
 
     if (dy < 0 && !this._gravityActive) {
+        /*
+        // Alternative idea: slightly variable x speed based off of dx, and
+        // calculating everything else from there
+        vx = dx*2; // This number's calculation needs fine-tuning
+        dt = dx/vx;
+        gravity = 2*-dy/(dt*dt);
+
+        this.gravityConst(gravity);
+        this.trigger('LiftedOffGround');
+
+        vy = -Math.sqrt(-dy*this.ay*2);
+        */
         this.trigger('LiftedOffGround');
         vy = -Math.sqrt(-dy*this.ay*2);
         dt = Math.sqrt(2*-dy/this.ay);
